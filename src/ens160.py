04afbce128efdfa9f8ded28fdf2e35805f7abcec
Bernhard Bablok <bablokb@gmx.de>
2023-07-07 08:45:43 +0200

    preliminary implementation

--- src/ens160.py
+++ src/ens160.py
@@ -18,13 +18,14 @@
 from log_writer import Logger
 g_logger = Logger()
 
-import adadruit_ens160
+import time
+import adafruit_ens160
 
 class ENS160:
-  formats = ["Status:", "{0}",
-             "AQI:", "{0}",
-             "TVOC:", "{0} ppb",
-             "eCO2:", "{0} ppm eq."
+  # don't print status on display, but add to csv
+  formats = ["AQI:", "{0}",
+             "TVOC:", "{0}",
+             "eCO2:", "{0}"
              ]
   headers = 'status,AQI,TVOC ppb,eCO2 ppm eq.'
 
@@ -43,15 +44,44 @@ class ENS160:
         g_logger.print("detected ens160 on i2c0")
 
   def read(self,data,values):
-    if "aht20" in data:
-      self.ens160.temperature_compensation = data["aht20"]["temp"]
-      self.ens160.humidity_compensation    = data["aht20"]["hum"]
-    ens_data           = self.ens160.read_all_sensors()
-    ens_data['status'] = self.ens160.data_validity
+    status = self.ens160.data_validity
 
-    data["ens160"] = ens_data
-    values.extend([None,ens_data['status']])
-    values.extend([None,ens_data['AQI']])
-    values.extend([None,ens_data['TVOC']])
-    values.extend([None,ens_data['eCO2']])
-    return f"{ens_data['status']},{ens_data['AQI']},{ens_data['TVOC']},{ens_data['eCO2']}"
+    # initial startup or invalid data
+    if status > 1:
+      g_logger.print("ens160: initial startup or invalid data!")
+      values.extend([None,status])
+      values.extend([None,0])
+      values.extend([None,0])
+      values.extend([None,0])
+      return f"{ens_data['status']},0,0,0"
+
+    # warmup
+    if status == 1:
+      sleep_time = 120
+      while status == 1:
+        g_logger.print(f"ens160: warmup - sleeping {sleep_time}s}")
+        time.sleep(sleep_time)
+        sleep_time = max(sleep_time/2,10)
+        status = self.ens160.data_validity
+
+    # normal operation
+    if status == 0:
+      g_logger.print("ens160: normal operation")
+      if "aht20" in data:
+        self.ens160.temperature_compensation = data["aht20"]["temp"]
+        self.ens160.humidity_compensation    = data["aht20"]["hum"]
+
+      #status == 0 might still not provide valid data
+      while True:
+        while not self.ens160.new_data_available:
+          g_logger.print("ens160: waiting for data...")
+          time.sleep(0.2)
+        ens_data = self.ens160.read_all_sensors()
+        if not ens_data['eCO2'] is None:
+          break
+
+      data["ens160"] = ens_data
+      values.extend([None,ens_data['AQI']])
+      values.extend([None,ens_data['TVOC']])
+      values.extend([None,ens_data['eCO2']])
+      return f"{status},{ens_data['AQI']},{ens_data['TVOC']},{ens_data['eCO2']}"
