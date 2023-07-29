4365441df482eca3aa0ac308faa7517860182211
Bernhard Bablok <bablokb@gmx.de>
2023-07-29 09:58:46 +0200

    fix: renamed am3220 to am2320

--- src/am2320.py
+++ src/am2320.py
@@ -1,9 +1,9 @@
 #-----------------------------------------------------------------------------
-# Sensor definition for AM3220.
+# Sensor definition for AM2320.
 #
 # Naming convention:
-#   - filenames in lowercase (am3220.py)
-#   - class name the same as filename in uppercase (AM3220)
+#   - filenames in lowercase (am2320.py)
+#   - class name the same as filename in uppercase (AM2320)
 #   - the constructor must take five arguments (config,i2c0,ic1,spi0,spi1)
 #     and probe for the device
 #   - i2c1 is the default i2c-device and should be probed first
@@ -18,7 +18,7 @@
 from log_writer import Logger
 g_logger = Logger()
 
-class AM3220:
+class AM2320:
   formats = ["T/AM:", "{0:.1f}°C","H/AM:", "{0:.0f}%rH"]
   headers = 'T/AM °C,H/AM %rH'
 
@@ -27,21 +27,21 @@ class AM3220:
     import adafruit_am2320
     try:
       if i2c1:
-        g_logger.print("testing am3220 on i2c1")
-        self.am3220 = adafruit_am2320.AM2320(i2c1)
-        g_logger.print("detected am3220 on i2c1")
+        g_logger.print("testing am2320 on i2c1")
+        self.am2320 = adafruit_am2320.AM2320(i2c1)
+        g_logger.print("detected am2320 on i2c1")
     except Exception as ex:
       g_logger.print(f"exception: {ex}")
       if i2c0:
-        g_logger.print("testing am3220 on i2c0")
-        self.am3220 = adafruit_am2320.AM2320(i2c0)
-        g_logger.print("detected am3220 on i2c0")
+        g_logger.print("testing am2320 on i2c0")
+        self.am2320 = adafruit_am2320.AM2320(i2c0)
+        g_logger.print("detected am2320 on i2c0")
 
   def read(self,data,values):
     """ read sensor """
-    t = self.am3220.temperature
-    h = self.am3220.relative_humidity
-    data["am3220"] = {
+    t = self.am2320.temperature
+    h = self.am2320.relative_humidity
+    data["am2320"] = {
       "temp": t,
       "hum":  h
     }
