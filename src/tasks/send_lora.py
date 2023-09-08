e08a1bac54c92f0a5cd1b1829a4eb83c57db90a6
Bernhard Bablok <bablokb@gmx.de>
2023-09-08 19:00:32 +0200

    fixed indentation and polished logging

--- src/tasks/send_lora.py
+++ src/tasks/send_lora.py
@@ -24,21 +24,21 @@ class LORA:
 
     # don't catch exceptions, main.py will handle that
 
-    g_logger.print("Setting up SPI1")
+    g_logger.print("LoRa: setting up SPI1")
     spi1 = busio.SPI(pins.PIN_LORA_SCK,pins.PIN_LORA_MOSI,pins.PIN_LORA_MISO)
     pin_cs                = DigitalInOut(pins.PIN_LORA_CS)
     pin_reset             = DigitalInOut(pins.PIN_LORA_RST)
     pin_enable            = DigitalInOut(pins.PIN_LORA_EN)
     pin_enable.direction  = Direction.OUTPUT
 
-    g_logger.print("Enabling rfm9x on SPI1")
+    g_logger.print("LoRa: enabling rfm9x")
     pin_enable.value = 1
     time.sleep(config.LORA_ENABLE_TIME)
-    g_logger.print("Initializing rfm9x on SPI1")
+    g_logger.print("LoRa: nitializing rfm9x")
     self.rfm9x = adafruit_rfm9x.RFM9x(
       spi1, pin_cs,pin_reset,config.LORA_FREQ,baudrate=100000)
       
-    g_logger.print("configuring rfm9x on spi")
+    g_logger.print("LoRa: configuring rfm9x")
     self.rfm9x.enable_crc = True
     #self.rfm9x.tx_power = 23
     self.rfm9x.ack_delay = 0.1
@@ -47,13 +47,16 @@ class LORA:
     self.rfm9x.ack_wait = 0.5
     self.rfm9x.ack_retries = 3
 
-    # --- transmit command   ----------------------------------------------
+  # --- transmit command   ----------------------------------------------
 
-    def transmit(self, header, string):
-      """ send data """
-      g_logger.print("LoRa transmit")
-      message = self.rfm9x.send_with_ack(bytes(string, "UTF-8"))
-      g_logger.print("Success? "+str(message))
+  def transmit(self, header, string):
+    """ send data """
+    g_logger.print("LoRa: sending data...")
+    send_ok = self.rfm9x.send_with_ack(bytes(string, "UTF-8"))
+    if send_ok:
+      g_logger.print("LoRa: ... successful")
+    else:
+      g_logger.print("LoRa: ... failed")
 
 def run(config,app):
   """ send data using LoRa """
