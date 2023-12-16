cd8550192c8861b435ba5006995989daceefc433
Bernhard Bablok <bablokb@gmx.de>
2023-12-16 14:00:57 +0100

    mount sd-card if available

--- src/admin.py
+++ src/admin.py
@@ -12,12 +12,13 @@ from digitalio import DigitalInOut, Pull, Direction
 
 import pins
 import webap
+from datacollector import Settings
 
 # access point settings
 try:
-  from ap_config import config
+  from ap_config import ap_config
 except:
-  config = {
+  ap_config = {
     'debug': True,
     'ssid': 'datalogger',
     'password': '12345678',             # ignored for wifi.AuthMode.OPEN
@@ -25,6 +26,8 @@ except:
     'hostname': 'datalogger'           # msdn hostname
   }
 
+g_config = Settings()
+g_config.import_config()
 
 # --- turn on LED on sensor-pcb   --------------------------------------------
 
@@ -32,11 +35,23 @@ switch_d = DigitalInOut(pins.PIN_SWD)
 switch_d.direction = Direction.OUTPUT
 switch_d.value = True
 
+# --- mount sd-card if available   -------------------------------------------
+
+if g_config.HAVE_SD:
+  import storage
+  import adafruit_sdcard
+  import busio
+  spi    = busio.SPI(pins.PIN_SD_SCK,pins.PIN_SD_MOSI,pins.PIN_SD_MISO)
+  sd_cs  = DigitalInOut(pins.PIN_SD_CS)
+  sdcard = adafruit_sdcard.SDCard(spi,sd_cs)
+  vfs    = storage.VfsFat(sdcard)
+  storage.mount(vfs, "/sd")
+
 # --- start AP and web-server   ----------------------------------------------
 
-if config["debug"]:
+if ap_config["debug"]:
   time.sleep(5)
   print("!!! Starting in ADMIN-Mode !!!")
 
-server = webap.WebAP(config=config)
+server = webap.WebAP(config=ap_config)
 server.run()
