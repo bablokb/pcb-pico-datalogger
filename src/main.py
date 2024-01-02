d585c2362d3adc9811458094d55d158f30b728cd
Bernhard Bablok <bablokb@gmx.de>
2024-01-02 16:36:19 +0100

    add info-message after exception

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
 #-----------------------------------------------------------------------------
 # Basic data-collection program. This program will
 #
-#   - initialize hardware
-#   - update RTCs (time-server->) external-RTC -> internal-RTC
-#   - collect data
-#   - update the display
-#   - save data
-#   - set next wakeup alarm
-#   - turn power off
+# The code is encapsulated in the class Datacollector, mainly to allow
+# the use of precompiled code.
 #
 # Author: Bernhard Bablok
 #
@@ -15,4 +10,15 @@
 #-----------------------------------------------------------------------------
 
 import datacollector
-datacollector.run()
+
+try:
+  app = datacollector.DataCollector()
+  app.run()
+except Exception as ex:
+  import traceback
+  traceback.print_exception(ex)
+  try:
+    app.cleanup()
+  except:
+    pass
+  print("please press the reset-button!")
