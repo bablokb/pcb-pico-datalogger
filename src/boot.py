a114b20537a846ef950a9d57c9767c1d9cbc7001
Bernhard Bablok <bablokb@gmx.de>
2024-02-13 14:04:34 +0100

    always blink on button-press, not only in test-mode

--- src/boot.py
+++ src/boot.py
@@ -17,22 +17,31 @@ from digitalio import DigitalInOut, Pull, Direction
 import pins
 from config import TEST_MODE
 
-# --- blink LED in test-mode   -----------------------------------------------
-
-if TEST_MODE:
-  switch_d = DigitalInOut(pins.PIN_SWD)
-  switch_d.direction = Direction.OUTPUT
-  for _ in range(3):
-    switch_d.value = True
-    time.sleep(0.15)
-    switch_d.value = False
-    time.sleep(0.15)
-
 # --- configure hardware   ---------------------------------------------------
 
 switch_a = DigitalInOut(pins.PIN_SWA)
 switch_a.direction = Direction.INPUT
 switch_a.pull = Pull.UP
+
+switch_b = DigitalInOut(pins.PIN_SWB)
+switch_b.direction = Direction.INPUT
+switch_b.pull = Pull.UP
+
+switch_c = DigitalInOut(pins.PIN_SWC)
+switch_c.direction = Direction.INPUT
+switch_c.pull = Pull.UP
+
+led_d = DigitalInOut(pins.PIN_SWD)
+led_d.direction = Direction.OUTPUT
+
+# --- blink LED on button-press   --------------------------------------------
+
+if TEST_MODE or not switch_a.value or not switch_b.value or not switch_c.value:
+  for _ in range(3):
+    led_d.value = True
+    time.sleep(0.15)
+    led_d.value = False
+    time.sleep(0.15)
 
 # --- check if switch A is pressed and if so, enter admin-mode   -------------
 
