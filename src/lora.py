4c927c5766e15f87f22b27719d3b706de7637815
Bernhard Bablok <bablokb@gmx.de>
2024-03-01 11:22:16 +0100

    changes to support both gateway and node role

--- src/lora.py
+++ src/lora.py
@@ -25,36 +25,41 @@ class LORA:
   def __init__(self,config):
     """ constructor """
 
+    self._config = config
     # don't catch exceptions, main.py will handle that
 
-    g_logger.print("LoRa: setting up SPI1")
+    g_logger.print("LoRa: setting up SPI")
     spi1 = busio.SPI(pins.PIN_LORA_SCK,pins.PIN_LORA_MOSI,pins.PIN_LORA_MISO)
     pin_cs                = DigitalInOut(pins.PIN_LORA_CS)
     pin_reset             = DigitalInOut(pins.PIN_LORA_RST)
-    pin_enable            = DigitalInOut(pins.PIN_LORA_EN)
-    pin_enable.direction  = Direction.OUTPUT
 
-    g_logger.print("LoRa: enabling rfm9x")
-    pin_enable.value = 1
-    time.sleep(config.LORA_ENABLE_TIME)
+    if hasattr(pins,'PIN_LORA_EN'):
+      g_logger.print("LoRa: enabling rfm9x")
+      pin_enable = DigitalInOut(pins.PIN_LORA_EN)
+      pin_enable.direction  = Direction.OUTPUT
+      pin_enable.value = 1
+      time.sleep(getattr(config,'LORA_ENABLE_TIME',0))
+
     g_logger.print("LoRa: initializing rfm9x")
     self.rfm9x = adafruit_rfm9x.RFM9x(
       spi1, pin_cs,pin_reset,config.LORA_FREQ,baudrate=100000)
-      
+
     g_logger.print("LoRa: configuring rfm9x")
     self.rfm9x.enable_crc = True
     self.rfm9x.tx_power = config.LORA_TX_POWER
-    self.rfm9x.node = config.LORA_NODE_ADDR                 # node or this device
-    self.rfm9x.destination = config.LORA_BASE_ADDR  # base station or destination
-    self.rfm9x.ack_wait = config.LORA_ACK_WAIT
-    self.rfm9x.ack_retries = config.LORA_ACK_RETRIES
+    self.rfm9x.node = config.LORA_NODE_ADDR                      # this
+    self.rfm9x.destination = getattr(config,"LORA_BASE_ADDR",0)  # gateway
+    self.rfm9x.ack_wait = getattr(config,'LORA_ACK_WAIT',0.25)
+    self.rfm9x.ACK_DELAY = getattr(config,"LORA_ACK_DELAY",0.1)
+    self.rfm9x.ack_retries = getattr(config,"LORA_ACK_RETRIES",3)
     self.rfm9x.sleep()
 
   # --- transmit command   ---------------------------------------------------
 
   def transmit(self,string,ack=True,keep_listening=False):
     """ send data """
-    g_logger.print("LoRa: sending data...")
+    if getattr(self._config,'TEST_MODE',False) or getattr(self._config,'DEV_MODE',False):
+      g_logger.print("LoRa: sending data...")
     if ack:
       return self.rfm9x.send_with_ack(bytes(string, "UTF-8"))
     else:
@@ -64,10 +69,18 @@ class LORA:
 
   def receive(self,with_ack=True,timeout=0.5):
     """ receive and decode data """
-    g_logger.print("LoRa: receiving data...")
+    if getattr(self._config,'TEST_MODE',False) or getattr(self._config,'DEV_MODE',False):
+      g_logger.print("LoRa: receiving data...")
     packet = self.rfm9x.receive(with_ack=with_ack,timeout=timeout)
     if packet is None:
-      return None
+      return (None,None,None)
     else:
-      g_logger.print(f"LoRa: packet: {packet.decode()}")
+      if getattr(self._config,'TEST_MODE',False) or getattr(self._config,'DEV_MODE',False):
+        g_logger.print(f"LoRa: packet: {packet.decode()}")
       return (packet.decode(),self.rfm9x.last_snr,self.rfm9x.last_rssi)
+
+  # --- set destination   ----------------------------------------------------
+
+  def set_destination(self,dest):
+    """ set destination for transmit """
+    self.rfm9x.destination = dest
