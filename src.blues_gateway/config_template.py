af03b9964651eecf3cddd842ede0236f010985e6
Bernhard Bablok <bablokb@gmx.de>
2024-03-05 14:35:24 +0100

    add more constants

--- src.blues_gateway/config_template.py
+++ src.blues_gateway/config_template.py
@@ -8,17 +8,33 @@
 # Website: https://github.com/pcb-pico-datalogger
 #-----------------------------------------------------------------------------
 
-# Development-mode (useful only with a connected console)
-DEV_MODE = False
+# ----------------------------------------------------------------------------
 
 # action to perform when receiving data
 #   None:  no action, just print to log
 #   False: buffer data to notecard, but don't sync
 #   True:  don't buffer data, sync to Notehub immediatly
 SYNC_BLUES_ACTION = True
-RECEIVE_TIMEOUT = 1.0       # single receive wait-time
 
 LORA_FREQ       = 433.0     # frequency 433, 868, 915
 LORA_NODE_ADDR  = 0         # gateway node-address
 LORA_ACK_DELAY  = 0.1       # delay time before sending ACK
 LORA_TX_POWER   = 23        # transmit power (max: 23)
+
+ACTIVE_WINDOW_START = 7     # active (online) window start hour
+ACTIVE_WINDOW_END   = 17    # active (online) window end hour
+                            # the gateway runs from 07:00-17:59
+
+# ----------------------------------------------------------------------------
+# technical constants, don't change unless you know why
+
+RECEIVE_TIMEOUT  = 1.0      # single receive wait-time
+GET_TIME_REPEATS = 3        # how many times card.time() is executed
+MAX_SYNC_TIME    = 120      # number of seconds to wait for a sync
+
+# ----------------------------------------------------------------------------
+# Development-mode (useful only for developers with a connected console)
+
+DEV_MODE   = False
+DEV_UPTIME = 300            # minimum uptime regardless of active window
+DEV_SLEEP  =  60            # max sleep-time in DEV_MODE
