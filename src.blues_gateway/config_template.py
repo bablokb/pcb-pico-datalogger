6b6dca7b2788bc2d9b5830c460b8f2b3a9fd1393
Bernhard Bablok <bablokb@gmx.de>
2024-03-05 16:01:48 +0100

    set default for MAX_SYNC_TIME to 300

--- src.blues_gateway/config_template.py
+++ src.blues_gateway/config_template.py
@@ -12,8 +12,8 @@
 
 # action to perform when receiving data
 #   None:  no action, just print to log
-#   False: buffer data to notecard, but don't sync
-#   True:  don't buffer data, sync to Notehub immediatly
+#   False: buffer data to notecard, sync after active window
+#   True:  don't buffer data, sync to Notehub immediately
 SYNC_BLUES_ACTION = True
 
 LORA_FREQ       = 433.0     # frequency 433, 868, 915
@@ -23,14 +23,14 @@ LORA_TX_POWER   = 23        # transmit power (max: 23)
 
 ACTIVE_WINDOW_START = 7     # active (online) window start hour
 ACTIVE_WINDOW_END   = 17    # active (online) window end hour
-                            # the gateway runs from 07:00-17:59
+                            # the gateway runs from <start>:00-<end>:59
 
 # ----------------------------------------------------------------------------
 # technical constants, don't change unless you know why
 
 RECEIVE_TIMEOUT  = 1.0      # single receive wait-time
 GET_TIME_REPEATS = 3        # how many times card.time() is executed
-MAX_SYNC_TIME    = 120      # number of seconds to wait for a sync
+MAX_SYNC_TIME    = 300      # number of seconds to wait for a sync
 
 # ----------------------------------------------------------------------------
 # Development-mode (useful only for developers with a connected console)
