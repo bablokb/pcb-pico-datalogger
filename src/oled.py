3f1ad95f8d26c695f1d04c4952404ba02c99bfd9
Bernhard Bablok <bablokb@gmx.de>
2024-05-04 15:25:43 +0200

    added clear() and show_text()

--- src/oled.py
+++ src/oled.py
@@ -8,6 +8,8 @@
 
 import displayio
 from adafruit_displayio_ssd1306 import SSD1306
+from terminalio import FONT
+from adafruit_display_text import label
 
 import pins
 
@@ -25,8 +27,41 @@ class OLED:
     display_bus = displayio.I2CDisplay(i2c1,device_address=int(address,16))
     self._display = SSD1306(display_bus,width=int(width),height=int(height))
 
-  # --- return size   ---------------------------------------------------------
+    self._lines = ["Starting..."]
+    if height == 32:
+      self._lines.extend([""]*2)
+    else:
+      self._lines.extend([""]*5)
+    group = displayio.Group()
+    self._text = label.Label(FONT,
+                             text=self._lines[0],
+                             color=0xFFFFFF,line_spacing=1.05,
+                             anchor_point=(0,0),x=0,y=4
+                             )
+    group.append(self._text)
+    self._display.show(group)
+
+  # --- return size   --------------------------------------------------------
 
   def get_size(self):
     """ return display-object """
     return self._display.width,self._display.height
+
+  # --- clear display   ------------------------------------------------------
+
+  def clear(self):
+    """ clear display """
+    self._lines = [""]*len(self._lines)
+    self._text.text = ""
+
+  # --- show text   ----------------------------------------------------------
+
+  def show_text(self,lines,row=0):
+    """ show text lines starting at the given row """
+
+    for line in lines:
+      if row >= len(self._lines):
+        break
+      self._lines[row] = line
+      row += 1
+    self._text.text = "\n".join(self._lines)
