2ffea5e7f50a06c1c726da930fdf1ffcf2f58f0c
Bernhard Bablok <bablokb@gmx.de>
2024-07-18 21:34:04 +0200

    use switches/led only if defined

--- src/boot.py
+++ src/boot.py
@@ -19,24 +19,39 @@ from config import TEST_MODE
 
 # --- configure hardware   ---------------------------------------------------
 
-switch_a = DigitalInOut(pins.PIN_SWA)
-switch_a.direction = Direction.INPUT
-switch_a.pull = Pull.UP
+if hasattr(pins,"PIN_SWA"):
+  switch_a = DigitalInOut(pins.PIN_SWA)
+  switch_a.direction = Direction.INPUT
+  switch_a.pull = Pull.UP
+else:
+  switch_a = None
 
-switch_b = DigitalInOut(pins.PIN_SWB)
-switch_b.direction = Direction.INPUT
-switch_b.pull = Pull.UP
+if hasattr(pins,"PIN_SWB"):
+  switch_b = DigitalInOut(pins.PIN_SWB)
+  switch_b.direction = Direction.INPUT
+  switch_b.pull = Pull.UP
+else:
+  switch_b = None
 
-switch_c = DigitalInOut(pins.PIN_SWC)
-switch_c.direction = Direction.INPUT
-switch_c.pull = Pull.UP
+if hasattr(pins,"PIN_SWC"):
+  switch_c = DigitalInOut(pins.PIN_SWC)
+  switch_c.direction = Direction.INPUT
+  switch_c.pull = Pull.UP
+else:
+  switch_c = None
 
-led_d = DigitalInOut(pins.PIN_SWD)
-led_d.direction = Direction.OUTPUT
+if hasattr(pins,"PIN_SWD"):
+  led_d = DigitalInOut(pins.PIN_SWD)
+  led_d.direction = Direction.OUTPUT
+else:
+  led_d = None
 
 # --- blink LED on button-press   --------------------------------------------
 
-if TEST_MODE or not switch_a.value or not switch_b.value or not switch_c.value:
+if led_d and (TEST_MODE or
+              (switch_a and not switch_a.value) or
+              (switch_b and not switch_b.value) or
+              (switch_c and not switch_c.value)):
   for _ in range(3):
     led_d.value = True
     time.sleep(0.15)
@@ -45,7 +60,7 @@ if TEST_MODE or not switch_a.value or not switch_b.value or not switch_c.value:
 
 # --- check if switch A is pressed and if so, enter admin-mode   -------------
 
-if not switch_a.value:
+if switch_a and not switch_a.value:
   # make flash writable
   storage.remount("/",False)
   supervisor.set_next_code_file("admin.py",sticky_on_reload=True)
@@ -53,6 +68,6 @@ if not switch_a.value:
 
 # --- check if switch B is pressed and if so, enter broadcast-mode   ---------
 
-if not switch_b.value:
+if switch_b and not switch_b.value:
   supervisor.set_next_code_file("broadcast.py",sticky_on_reload=True)
   supervisor.reload()
