0e970066a27eba95cf8f5a9444f54d9ec7ad6074
Bernhard Bablok <bablokb@gmx.de>
2024-07-19 13:06:17 +0200

    support active-high buttons

--- src/boot.py
+++ src/boot.py
@@ -15,30 +15,39 @@ import supervisor
 from digitalio import DigitalInOut, Pull, Direction
 
 import pins
-from config import TEST_MODE
+try:
+  from config import TEST_MODE
+except:
+  TEST_MODE = False
 
 # --- configure hardware   ---------------------------------------------------
 
 if hasattr(pins,"PIN_SWA"):
   switch_a = DigitalInOut(pins.PIN_SWA)
   switch_a.direction = Direction.INPUT
-  switch_a.pull = Pull.UP
+  switch_a.pull = Pull.UP if pins.PIN_SWA_ACTIVE_LOW else Pull.DOWN
+  a_pressed = lambda: (
+    not switch_a.value if pins.PIN_SWA_ACTIVE_LOW else switch_a.value)
 else:
-  switch_a = None
+  a_pressed = lambda: False
 
 if hasattr(pins,"PIN_SWB"):
   switch_b = DigitalInOut(pins.PIN_SWB)
   switch_b.direction = Direction.INPUT
-  switch_b.pull = Pull.UP
+  switch_b.pull = Pull.UP if pins.PIN_SWB_ACTIVE_LOW else Pull.DOWN
+  b_pressed = lambda: (
+    not switch_b.value if pins.PIN_SWB_ACTIVE_LOW else switch_b.value)
 else:
-  switch_b = None
+  b_pressed = lambda: False
 
 if hasattr(pins,"PIN_SWC"):
   switch_c = DigitalInOut(pins.PIN_SWC)
   switch_c.direction = Direction.INPUT
-  switch_c.pull = Pull.UP
+  switch_c.pull = Pull.UP if pins.PIN_SWC_ACTIVE_LOW else Pull.DOWN
+  c_pressed = lambda: (
+    not switch_c.value if pins.PIN_SWC_ACTIVE_LOW else switch_c.value)
 else:
-  switch_c = None
+  c_pressed = lambda: False
 
 if hasattr(pins,"PIN_SWD"):
   led_d = DigitalInOut(pins.PIN_SWD)
@@ -48,10 +57,7 @@ else:
 
 # --- blink LED on button-press   --------------------------------------------
 
-if led_d and (TEST_MODE or
-              (switch_a and not switch_a.value) or
-              (switch_b and not switch_b.value) or
-              (switch_c and not switch_c.value)):
+if led_d and (TEST_MODE or a_pressed() or b_pressed() or c_pressed()):
   for _ in range(3):
     led_d.value = True
     time.sleep(0.15)
@@ -60,7 +66,7 @@ if led_d and (TEST_MODE or
 
 # --- check if switch A is pressed and if so, enter admin-mode   -------------
 
-if switch_a and not switch_a.value:
+if a_pressed():
   # make flash writable
   storage.remount("/",False)
   supervisor.set_next_code_file("admin.py",sticky_on_reload=True)
@@ -68,6 +74,6 @@ if switch_a and not switch_a.value:
 
 # --- check if switch B is pressed and if so, enter broadcast-mode   ---------
 
-if switch_b and not switch_b.value:
+if b_pressed():
   supervisor.set_next_code_file("broadcast.py",sticky_on_reload=True)
   supervisor.reload()
