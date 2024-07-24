948b2c284bce35f70153f2d4e750abcda2adbaf4
Bernhard Bablok <bablokb@gmx.de>
2024-07-24 10:47:53 +0200

    fix: window-start and window-end must be strings

--- src.blues_gateway/config_template.py
+++ src.blues_gateway/config_template.py
@@ -21,8 +21,8 @@ LORA_NODE_ADDR  = 0         # gateway node-address
 LORA_ACK_DELAY  = 0.1       # delay time before sending ACK
 LORA_TX_POWER   = 23        # transmit power (max: 23)
 
-ACTIVE_WINDOW_START = 7:00  # active (online) window start time
-ACTIVE_WINDOW_END   = 17:00 # active (online) window end time
+ACTIVE_WINDOW_START = "7:00"  # active (online) window start time
+ACTIVE_WINDOW_END   = "17:00" # active (online) window end time
 
 # ----------------------------------------------------------------------------
 # technical constants, don't change unless you know why
