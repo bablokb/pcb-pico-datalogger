2ae5036a8223e222668505cdc03f2e1e58a2ed9c
Bernhard Bablok <bablokb@gmx.de>
2024-07-25 10:20:23 +0200

    fix for boards that don't provide VOLTAGE_MONITOR

--- src/sensors/battery.py
+++ src/sensors/battery.py
@@ -23,15 +23,20 @@ class BATTERY:
 
   def __init__(self,config,i2c,addr=None,spi=None):
     """ constructor """
-    self.ignore = False
 
   def read(self,data,values):
     """ read voltage monitor """
 
-    adc = AnalogIn(board.VOLTAGE_MONITOR)
-    level = round(adc.value *  3 * 3.3 / 65535,2)
-    adc.deinit()
+    if hasattr(board,"VOLTAGE_MONITOR"):
+      adc = AnalogIn(board.VOLTAGE_MONITOR)
+      level = round(adc.value *  3 * 3.3 / 65535,2)
+      adc.deinit()
+      if level < 1.8 or level > 5.5:
+        # this happens only if the voltage-monitor is external and not connected
+        # in this case, fake the level to prevent triggering level-based logic
+        level = 3.5
+    else:
+      level = 3.5
     data["battery"] = level
-    if not self.ignore:
-      values.extend([None,level])
+    values.extend([None,level])
     return f"{level:0.2f}"
