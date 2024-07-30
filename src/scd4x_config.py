e355d3c49848a07f0fbc2c954df902c5f0765419
Bernhard Bablok <bablokb@gmx.de>
2024-07-30 20:59:36 +0200

    add wrapper ('main program') for tools/scd4x_config.py

---
+++ src/scd4x_config.py
@@ -0,0 +1,29 @@
+#-----------------------------------------------------------------------------
+# Start SCD4x autoconfiguration
+#
+# Author: Bernhard Bablok
+#
+# Website: https://github.com/pcb-pico-datalogger
+#-----------------------------------------------------------------------------
+
+import time
+from digitalio import DigitalInOut, Direction
+import pins
+
+# --- turn on LED on sensor-pcb   --------------------------------------------
+
+if hasattr(pins,"PIN_SWD"):
+  switch_d = DigitalInOut(pins.PIN_SWD)
+  switch_d.direction = Direction.OUTPUT
+  switch_d.value = True
+elif hasattr(board,'LED'):
+  switch_d = DigitalInOut(board.LED)
+  switch_d.direction = Direction.OUTPUT
+  switch_d.value = True
+
+
+time.sleep(5)
+print("starting scd4x_config.autorun()")
+
+from tools import scd4x_config
+scd4x_config.autorun()
