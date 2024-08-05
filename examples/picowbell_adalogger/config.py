48b86cb1fbfdc1ce6b2f8f7a18459def5f13ecf0
Bernhard Bablok <bablokb@gmx.de>
2024-08-05 13:36:45 +0200

    cleanup example

--- examples/picowbell_adalogger/config.py
+++ examples/picowbell_adalogger/config.py
@@ -1,32 +1,34 @@
 #-----------------------------------------------------------------------------
 # Configuration constants for main.py.
 #
-# Example using a PiCowbell-Adalogger without display.
+# Using PiCowbell-Adalogger and no display.
 #
 # Author: Bernhard Bablok
 #
 # Website: https://github.com/pcb-pico-datalogger
 #-----------------------------------------------------------------------------
 
-NET_UPDATE  = True        # update RTC from time-server if time is invalid
+STROBE_MODE = False
+INTERVAL    = 10
 
-STROBE_MODE = True
-INTERVAL    = 60
-
-HAVE_SD       = True
+HAVE_SD       = False
 HAVE_DISPLAY  = None
 HAVE_RTC      = "PCF8523(0)"
 HAVE_I2C0     = True
-HAVE_PM       = True
-SHUTDOWN_HIGH = True          # use TPL5110/TPL5111
+HAVE_PM       = False
+SHUTDOWN_HIGH = True          # in case TPL5110/TPL5111 is used
 
-SENSORS      = "id battery cputemp"
-CSV_FILENAME = f"/sd/data-{{ID}}-{SENSORS.replace(' ','_')}.csv"
+HAVE_OLED = "0,0x3c,128,64"
+OLED_VALUES = "pms5003(PM0.3:) pms5003(PM1.0:)"
+
+SENSORS       = "id battery pms5003"
+CSV_FILENAME = f"/sd/particle-data.csv"
 
 LOGGER_NAME  = 'adalogger'
 LOGGER_ID    = 'ADA_1'
 LOGGER_LOCATION = '@Home'
+LOGGER_TITLE = f'{LOGGER_ID}: {LOGGER_NAME}'
 
 # tasks to execute after data-collection
-TASKS = "dump_data save_data"
+TASKS = "dump_data save_data update_oled"
 SHOW_UNITS = True
