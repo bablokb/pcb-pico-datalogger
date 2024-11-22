2e0400b98808850d0045c7e3d968a34514cb9b82
Bernhard Bablok <bablokb@gmx.de>
2024-11-22 08:43:57 +0100

    create noop gateway sender

---
+++ src.gateway/gw_tx_noop.py
@@ -0,0 +1,53 @@
+#-----------------------------------------------------------------------------
+# Noop gateway sender class.
+#
+# Author: Bernhard Bablok
+#
+# Website: https://github.com/pcb-pico-datalogger
+#-----------------------------------------------------------------------------
+
+# --- early configuration of the log-destination   ---------------------------
+
+from log_writer import Logger
+g_logger = Logger()
+
+# --- BluesSender class   ----------------------------------------------------
+
+class NoopSender:
+  """ NoopSender class """
+
+  # --- constructor   --------------------------------------------------------
+
+  def __init__(self,config):
+    """ constructor """
+    self._config = config
+
+  # --- hardware-setup   -----------------------------------------------------
+
+  def setup(self,i2c,spi):
+    """ initialize hardware """
+    g_logger.print(f"NoopSender: initializing")
+
+  # --- get time from upstream   ---------------------------------------------
+
+  def get_time(self):
+    """ query time: return time-stamp or None """
+    return None
+
+  # --- process data   -------------------------------------------------------
+
+  def process_data(self,values):
+    """ process data  """
+
+    g_logger.print("NoopSender: processing sensor-data...")
+    start = time.monotonic()
+    duration = time.monotonic()-start
+    g_logger.print(f"NoopSender: duration: {duration}s")
+
+  # --- shutdown   -----------------------------------------------------------
+
+  def shutdown(self,s_time):
+    """ Shutdown system. In our case, send an attn-request to the notecard """
+
+    g_logger.print("NoopSender: shutdown...")
+    return False
