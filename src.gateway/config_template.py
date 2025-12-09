a2de15e46e194078e37f3debc586a8a108b9e25c
Bernhard Bablok <bablokb@gmx.de>
2025-12-09 10:32:47 +0100

    updated configuration template

--- src.gateway/config_template.py
+++ src.gateway/config_template.py
@@ -8,6 +8,13 @@
 # Website: https://github.com/pcb-pico-datalogger
 #-----------------------------------------------------------------------------
 
+# --- GW identification   ------------------------------------------------
+
+GW_NAME     = 'My Gateway'
+GW_ID       = 'xxx'            # any string (without commas)
+GW_LOCATION = 'yyy'            # use e.g. Plus Code (no commas)
+GW_TITLE    = 'zzz'            # used for the display
+
 # ----------------------------------------------------------------------------
 # hardware configuration
 HAVE_RTC   = None
@@ -21,13 +28,23 @@ LORA_FREQ       = 433.0     # frequency 433, 868, 915
 LORA_NODE_ADDR  = 0         # gateway node-address
 LORA_ACK_DELAY  = 0.1       # delay time before sending ACK
 LORA_TX_POWER   = 23        # transmit power (max: 23)
+RECEIVE_TIMEOUT  = 1.0      # single receive wait-time
 
 # ----------------------------------------------------------------------------
+# gw_tx_blues specific configuration
+#
 # action to perform when receiving data
 #   None:  no action, just print to log
 #   False: buffer data to notecard, sync after active window
 #   True:  don't buffer data, sync to Notehub immediately
 SYNC_BLUES_ACTION = True
+GET_TIME_REPEATS = 3        # how many times card.time() is executed
+MAX_SYNC_TIME    = 300      # number of seconds to wait for a sync
+
+# --- tasks to execute after receiving data  - -------------------------------
+# See the docs for a complete list of tasks and for special task configuration.
+
+TASKS = "save_data update_oled tx_send"
 
 # ----------------------------------------------------------------------------
 # uptime configuration. The template values configure a single active window
@@ -45,13 +62,6 @@ TIME_TABLE = [
   ((7,7,1),(0,0,1))
   ]
 ON_DURATION = 600   # 600 = 10*60
-
-# ----------------------------------------------------------------------------
-# technical constants, don't change unless you know why
-
-RECEIVE_TIMEOUT  = 1.0      # single receive wait-time
-GET_TIME_REPEATS = 3        # how many times card.time() is executed
-MAX_SYNC_TIME    = 300      # number of seconds to wait for a sync
 
 # ----------------------------------------------------------------------------
 # Development-mode (useful only for developers with a connected console)
