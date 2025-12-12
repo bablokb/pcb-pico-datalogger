7de6fcf120a2725614f6581a71a9941b88dd0d31
Bernhard Bablok <bablokb@gmx.de>
2025-12-12 07:29:45 +0100

    added broadcast-task bc_send_udp

---
+++ src.gateway/tasks/bc_send_udp.py
@@ -0,0 +1,22 @@
+#-----------------------------------------------------------------------------
+# Gateway-task: send broadcast-info to UDP-destination
+#
+# Author: Bernhard Bablok
+#
+# Website: https://github.com/pcb-pico-datalogger
+#-----------------------------------------------------------------------------
+
+import time
+
+from log_writer import Logger
+g_logger = Logger()
+
+from wifi_impl_builtin import WifiImpl
+
+def run(config, app, values):
+  """ send broadcast-info to UDP-destination """
+  wifi = WifiImpl()
+  host = config.BC_UDP_HOST
+  port = config.BC_UDP_PORT
+  g_logger.print(f"gateway: sending broadcast data to {host}:{port}...")
+  wifi.sendto(bytes(','.join(values)+'\n',"UTF-8"),host,port)
